9cc2f17ebf94e4b97943fedb394887b797e5aef0
Etienne Dx <etiennedx00@gmail.com>
2022-10-18 15:09:37 +0200

    feat: Added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -5,6 +7,20 @@ import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { theme } from './style/theme';
+
+
+Sentry.init({
+  dsn: "https://ce985e51e56d4e0dad01f16317577e76@o4504004750016512.ingest.sentry.io/4504004751654912",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  debug: true,
+  enabled: true,
+  environment: process.env.NODE_ENV,
+});
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
